813a95e6d7aaba2581a3b683ea4aab5bcc82fb0c
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-08-26 15:11:40 +0100

    Create communicator.js

---
+++ communicator.js
@@ -0,0 +1,10 @@
+// When you run the f12 console.js script, it downloads this script and adds it to the DOM of the Scratch project (temporarily, you have to re-paste after reloading the page).
+
+window.setInterval(checkForRequests(), 1000);
+
+checkForRequests() {
+  TODO: support request queueing
+  if (document.getElementByClassName("monitor_value_3Yexa").innerHTML == "-1") { // I think this means ALL shown variables, so TODO: change later
+    alert("Test request recieved");
+  }
+}
