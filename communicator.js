a682ae8ee117b1c140c0034f5660610e4fea6f55
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-08-26 15:16:34 +0100

    Update communicator.js

--- communicator.js
+++ communicator.js
@@ -4,7 +4,7 @@ window.setInterval(checkForRequests(), 1000);
 
 function checkForRequests() {
 // TODO: support request queueing
-  if (document.getElementByClassName("monitor_value_3Yexa").innerHTML == "-1") { // I think this means ALL shown variables, so TODO: change later
+  if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "-1") { // I think this means ALL shown variables, so TODO: change later
     alert("Test request recieved");
   }
 }
