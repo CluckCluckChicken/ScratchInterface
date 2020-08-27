91a5f7ccd39fbdf9b8ad05ec81533c309899871f
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-08-27 09:29:55 +0100

    Update communicator.js

--- communicator.js
+++ communicator.js
@@ -1,10 +1,10 @@
 // When you run the f12 console.js script, it downloads this script and adds it to the DOM of the Scratch project (temporarily, you have to re-paste after reloading the page).
 
-window.setInterval(function() {checkForRequests()}, 100);
+window.setInterval(function() {checkForRequests()}, 10);
 
 function checkForRequests() {
 // TODO: support request queueing
-  if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "10") { // I think this means ALL shown variables, so TODO: change later
+  if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "1") { // I think this means ALL shown variables, so TODO: change later
     window.open("https://www.google.com/", '_blank');
   }
 }
