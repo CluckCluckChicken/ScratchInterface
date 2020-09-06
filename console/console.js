e3c12d20135a2875163ed400ba4f9a06f6d0e801
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-09-06 12:41:05 +0100

    Update console.js

--- console/console.js
+++ console/console.js
@@ -6,21 +6,21 @@ function getCode() {
   db.collection("functions").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
-        docId++
-        console.log("foreach document");
+        docId++;
     });
   }).then(function() { writeCode(); });
-  
-  docId++;
 }
 
 function writeCode() {
+  docId++;
+  
   // Create the document with that function
   db.collection("functions").doc(docId.toString()).set({
     javascript: document.getElementById("function").value
   })
   .then(function() {
     console.log("Document successfully written!");
+    document.getElementById("outputCode").innerHTML == docId;
   })
   .catch(function(error) {
     console.error("Error writing document: ", error);
