b76c4b15c25a5d7598c4db1c845bf4755e55eca0
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-09-06 16:10:48 +0100

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,3 @@
 // This is the code that you paste into the f12 console on the Scratch project
 
-(function(){var po=document.createElement('script');po.type='text/javascript';po.async=true;po.src='https://scratchinterface.netlify.app/communicator.js';var s=document.getElementsByTagName('script')[0];document.body.insertBefore(po,document.body.childNodes[0]);})();
+(function(){var po=document.createElement('script');po.type='text/javascript';po.async=true;po.src='https://scratchinterface.netlify.app/loader.js';var s=document.getElementsByTagName('script')[0];document.body.insertBefore(po,document.body.childNodes[0]);})();
