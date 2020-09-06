4672cb9c13af850f384c4674309b585a22a846df
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-09-06 15:14:07 +0100

    Update communicator.js

--- communicator.js
+++ communicator.js
@@ -2,15 +2,26 @@
 
 window.setInterval(function() {checkForRequests()}, 10);
 
+var lock = true;
+
 function checkForRequests() {
-// TODO: support request queueing
-  if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "1") { // I think this means ALL shown variables, so TODO: change later
-    window.open("https://www.google.com/", '_blank');
+  if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "0") {
+    lock = false;
   }
-  if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "2") { // I think this means ALL shown variables, so TODO: change later
-    alert("follow potatophant!");
-  }
-  if (Number(document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML) < 0) { // I think this means ALL shown variables, so TODO: change later
-    alert("Custom JS functionality coming within the next few hours!");
+  
+  if (!lock) {
+    // TODO: support request queueing
+    if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "1") { // I think this means ALL shown variables, so TODO: change later
+      window.open("https://www.google.com/", '_blank');
+      lock = true;
+    }
+    if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "2") { // I think this means ALL shown variables, so TODO: change later
+      alert("follow potatophant!");
+      lock = true;
+    }
+   if (Number(document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML) < 0) { // I think this means ALL shown variables, so TODO: change later
+      alert("Custom JS functionality coming within the next few hours!");
+      lock = true;
+   }
   }
 }
