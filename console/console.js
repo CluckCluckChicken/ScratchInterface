af43316a5622a6a073d0f5b099e5efef05f400bd
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-09-06 11:23:36 +0100

    Update console.js

--- console/console.js
+++ console/console.js
@@ -1,20 +1,12 @@
 function getCode() {
-  var docId = 1;
-  var found = false;
-  while (found == false) {
-    var docRef = db.collection("functions").doc(docId.toString());
-    docRef.get().then(function(doc) {
-      if (doc.exists) {
-        docId++;
-      } else {
-        // doc.data() will be undefined in this case
-        found = true;
-        break;
-      }
-    }).catch(function(error) {
-      console.log("Error getting document:", error);
+  var docId = 0;
+  
+  db.collection("functions").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        docId++
     });
-  }
+  });
   
   // Create the document with that function
   db.collection("functions").doc(docId.toString()).set({
