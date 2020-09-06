3127334ce899923fa6012ad41d96a78983b8e8a0
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-09-06 16:12:03 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,3 +1,5 @@
+// When you run the f12 console.js script, it downloads this script and adds it to the DOM of the Scratch project (temporarily, you have to re-paste after reloading the page).
+
 (function() {
   var po = document.createElement('script');
   po.type = 'text/javascript';
@@ -14,60 +16,11 @@
   var s = document.getElementsByTagName('script')[0];
   document.body.insertBefore(po, document.body.childNodes[0]);
 })();
-
-// When you run the f12 console.js script, it downloads this script and adds it to the DOM of the Scratch project (temporarily, you have to re-paste after reloading the page).
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyCr-csz6B6AT-eXwjnXL5rppMM0gEIbpQQ",
-  authDomain: "scratchinterface.firebaseapp.com",
-  databaseURL: "https://scratchinterface.firebaseio.com",
-  projectId: "scratchinterface",
-  storageBucket: "scratchinterface.appspot.com",
-  messagingSenderId: "316602045699",
-  appId: "1:316602045699:web:0eff20e23bab8f8ee3c536"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var db = firebase.firestore();
-
-window.setInterval(function() {checkForRequests()}, 10);
-
-var lock = true; // Lock makes it more reliable
-
-function checkForRequests() {
-  if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "0") {
-    lock = false;
-  }
-  
-  if (!lock) {
-    // TODO: support request queueing
-    if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "1") { // I think this means ALL shown variables, so TODO: change later
-      window.open("https://www.google.com/", '_blank');
-      lock = true;
-    }
-    if (document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML == "2") { // I think this means ALL shown variables, so TODO: change later
-      alert("follow potatophant!");
-      lock = true;
-    }
-    if (Number(document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML) < 0) { // I think this means ALL shown variables, so TODO: change later
-      importCustomFunction(document.getElementsByClassName("monitor_value_3Yexa")[0].innerHTML);
-      lock = true;
-    }
-  }
-}
-
-function importCustomFunction(functionID) {
-  var docRef = db.collection("functions").doc("functionID.toString()");
-  docRef.get().then(function(doc) {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-  }).catch(function(error) {
-    console.log("Error getting document:", error);
-  });
-}
+(function() {
+  var po = document.createElement('script');
+  po.type = 'text/javascript';
+  po.async = true;
+  po.src = 'https://scratchinterface.netlify.app/communicator.js';
+  var s = document.getElementsByTagName('script')[0];
+  document.body.insertBefore(po, document.body.childNodes[0]);
+})();
