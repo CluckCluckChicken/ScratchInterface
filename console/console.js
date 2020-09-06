71047f26af0af7d342ef02eec48d259f03813922
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2020-09-06 11:03:01 +0100

    Create console.js

---
+++ console/console.js
@@ -0,0 +1,28 @@
+getCode() {
+  var docId = 1;
+  while (var found == false) {
+    var docRef = db.collection("functions").doc(docId.toString());
+    docRef.get().then(function(doc) {
+      if (doc.exists) {
+        docId++;
+      } else {
+        // doc.data() will be undefined in this case
+        found = true;
+      }
+      }).catch(function(error) {
+        console.log("Error getting document:", error);
+      });
+    }
+  }
+  
+  // Create the document with that function
+  db.collection("functions").doc(docId.toString()).set({
+    javascript: document.getElementById("function").value
+  })
+  .then(function() {
+    console.log("Document successfully written!");
+  })
+  .catch(function(error) {
+    console.error("Error writing document: ", error);
+  });
+}
